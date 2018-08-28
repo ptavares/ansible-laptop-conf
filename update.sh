d4a3ae6edc1884b477f689f3b836f5b5faeaebb5
Patrick Tavares <tavarespatrick01@gmail.com>
2018-08-28 20:34:05 +0200

    refacto for simpliest use

--- update.sh
+++ update.sh
@@ -1,9 +1,35 @@
 #!/bin/bash -e
+SCRIPT=`realpath -s $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+CURRENT_DIR=`pwd`
+
+echo $SCRIPT_PATH
 
 function log {
     echo `$DATE`" === $1"
 }
 
-log "Update system"
-ansible-playbook playbooks/update-laptop.yml --tags "system, manage-system-update, manage-system-clean"
+function callPlaybook {
+    cd ${SCRIPT_PATH}
+    ansible-playbook playbooks/init-laptop.yml --tags "$1"
+    cd ${CURRENT_DIR}
+}
 
+case $1 in
+    update)
+        log "Update system"
+        callPlaybook "manage-system-update, manage-system-clean"
+        ;;
+    zshConfig)
+        log "Update zsh config"
+        callPlaybook "zsh-install-conf"
+        ;;
+    updateDockerCompose)
+        log "Update docker-compose"
+        callPlaybook "docker-install-compose"
+        ;;
+    *)
+        echo "usage ${0} [update|zshConfig|updateDockerCompose]"
+        exit 1
+        ;;
+esac
